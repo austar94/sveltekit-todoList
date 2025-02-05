d0551ff220f84beaaff8ab894d37907eb6a88045
austar94 <48815765+austar94@users.noreply.github.com>
2025-02-05 14:26:22 +0900

    배포용 설정 추가

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,17 @@
-module.exports = {
-    content: ['./src/**/*.{svelte,js,ts}'],
-    plugins: [require('daisyui')],
-  };
-  
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			colors: {
+				'fire' : '#E71515',
+				'warning' : '#FF6C2E',
+				'disconnection' : '#FEB821',
+				'changing' : '#234B89',
+			}
+		}
+	},
+
+	plugins: [require('daisyui')],
+};
